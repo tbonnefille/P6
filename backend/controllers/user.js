26d5cf6ed79b5670f6763ac6b837ea6dc8579343
tbonnefille <bonnefille.thomas@gmail.com>
2022-10-26 12:05:38 +0200

    Corrctions

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -36,7 +36,7 @@ exports.login = (req, res, next) => {
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'process.env.SECRET',
+                            process.env.SECRET,
                             { expiresIn: '24h' }
                         )
                     });
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
    try {
        const token = req.headers.authorization.split(' ')[1];
-       const decodedToken = jwt.verify(token,'process.env.SECRET');
+       const decodedToken = jwt.verify(token,process.env.SECRET);
        const userId = decodedToken.userId;
        req.auth = {
            userId: userId
